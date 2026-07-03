7fa2369d4ae9f3ddd4903d33e2b8b706460bb31d
Matthew Cochran <matthew@cochranweb.com>
2026-07-03 10:55:56 -0400

    style: cargo fmt after edition-2024 migration

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod patch;
 pub mod projection;
 pub mod section;
 
-pub use outline::{outline, Heading};
-pub use patch::{delete_section, replace_section, PatchError};
-pub use projection::{project, SectionMeta};
-pub use section::{read_section, SectionError};
+pub use outline::{Heading, outline};
+pub use patch::{PatchError, delete_section, replace_section};
+pub use projection::{SectionMeta, project};
+pub use section::{SectionError, read_section};
